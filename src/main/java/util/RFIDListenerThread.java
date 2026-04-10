07553694e37ce5e58372cf463595917a4db74d88
Sulthan Khansa <sulthankhansa11@gmail.com>
2026-04-10 10:28:15 +0700

    Add multithreading for RFID listener

--- src/main/java/util/RFIDListenerThread.java
+++ src/main/java/util/RFIDListenerThread.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author user
- */
-public class RFIDListenerThread {
+public class RFIDListenerThread extends Thread {
     
+    public RFIDListenerThread() {
+        // Ini kuncinya: setDaemon(true)
+        // Artinya thread ini cuma "pembantu". Kalo aplikasi utama di-close, thread ini bakal otomatis ikut mati
+        setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Background Thread: Scanner RFID udah standby nunggu tap wkwk");
+        
+        while (true) {
+            try {
+                // Di sini nanti lu masukin kodingan buat ngebaca input dari USB Serial (Pertemuan 13)
+                // Sementara kita kasih jeda 3 detik aja buat simulasi
+                Thread.sleep(3000); 
+                
+            } catch (InterruptedException e) {
+                System.out.println("Background Thread dimatiin paksa.");
+                break;
+            }
+        }
+    }
 }
